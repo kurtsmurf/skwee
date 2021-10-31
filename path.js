e9a770efbfa383dd461d24276df51c974a1341f2
Kurt <chrisronsmith@gmail.com>
2021-10-31 12:39:18 -0400

    Fix path

--- path.js
+++ path.js
@@ -1,14 +1,16 @@
 // @ts-check
 
 const move = (path, { time, amplitude }) =>
-  path + `M ${time}, ${(amplitude + 0.5) * 100} `;
+  path + `M ${time}, ${ amplitude } `;
 const lineTo = (path, { time, amplitude }) =>
-  path + `L ${time}, ${(amplitude + 0.5) * 100} `;
+  path + `L ${time}, ${ amplitude } `;
+
+const normalize = float => (float + 0.5) * 100;
 
 export const toPath = (floats) => {
-  const [first, ...rest] = floats;
+  const [first, ...rest] = floats.map(normalize);
   const startingPoint = move("", { time: 0, amplitude: first });
   const reducer = (path, float, index) =>
-    lineTo(path, { time: index, amplitude: float });
+    lineTo(path, { time: index + 1, amplitude: float });
   return rest.reduce(reducer, startingPoint);
 };
