89edadff66ccc70d2471289a67300ddf26a0d4c7
Kurt <chrisronsmith@gmail.com>
2021-10-31 13:31:57 -0400

    fiddle

--- index.js
+++ index.js
@@ -2,27 +2,22 @@
 import { toPath } from "./path.js";
 
 const audioContext = new AudioContext();
-const analyser = audioContext.createAnalyser();
-const target = document.querySelector("[scope-mode]");
-analyser.fftSize = 128;
-const samplesHolder = new Float32Array(analyser.fftSize);
-
 document.body.addEventListener("click", () => audioContext.resume());
+
+const analyser = audioContext.createAnalyser();
+analyser.fftSize = 128;
+
+const samplesHolder = new Float32Array(analyser.fftSize);
+const path = document.querySelector("[scope-path]");
 
 const loop = () => {
   analyser.getFloatTimeDomainData(samplesHolder);
-  const target = document.querySelector("[scope-path]");
-  target.setAttribute("d", toPath(samplesHolder));
+  path.setAttribute("d", toPath(samplesHolder.slice(-100)));
   requestAnimationFrame(loop);
 };
 
-const main = async () => {
-  await navigator.mediaDevices
-    .getUserMedia({ audio: true, video: false })
-    .then((stream) => audioContext.createMediaStreamSource(stream))
-    .then((streamSource) => streamSource.connect(analyser));
-
-  requestAnimationFrame(loop);
-};
-
-main();
+navigator.mediaDevices
+  .getUserMedia({ audio: true, video: false })
+  .then((stream) => audioContext.createMediaStreamSource(stream))
+  .then((streamSource) => streamSource.connect(analyser))
+  .then(loop);
