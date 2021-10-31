3bab64f833eedc08d8e70811445689bf51cc6cdd
Kurt <chrisronsmith@gmail.com>
2021-10-31 14:51:53 -0400

    Add start/stop

--- index.js
+++ index.js
@@ -8,18 +8,28 @@ analyser.fftSize = 128;
 const samplesHolder = new Float32Array(analyser.fftSize);
 const scopePath = document.querySelector("[scope-path]");
 
-const loop = () => {
+const draw = () => {
   analyser.getFloatTimeDomainData(samplesHolder);
   scopePath.setAttribute("d", toPath(samplesHolder.slice(-100)));
+};
+
+const loop = () => {
+  draw();
   requestAnimationFrame(loop);
 };
 
-const addStartCapability = () =>
-  document.body.addEventListener("click", () => audioContext.resume());
+const startStop = () => {
+  if (audioContext.state === "suspended") return audioContext.resume();
+  if (audioContext.state === "running") return audioContext.suspend();
+};
+
+const interactive = () => {
+  document.body.addEventListener("click", startStop);
+};
 
 navigator.mediaDevices
   .getUserMedia({ audio: true, video: false })
   .then((stream) => audioContext.createMediaStreamSource(stream))
   .then((streamSource) => streamSource.connect(analyser))
   .then(loop)
-  .then(addStartCapability);
+  .then(interactive);
