7b49618452e3296bd00887e7ce556b0b2340add6
Chris Smith <chrisronsmith@gmail.com>
2021-11-06 21:46:54 -0400

    Apply gradient to stroke

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ const loop = () => {
   requestAnimationFrame(loop);
 };
 
-let str;
-
 const startStop = () => {
-  console.log("streamSource: " + str)
-  console.log("audioContext.state: " + audioContext.state)
   if (audioContext.state === "suspended") return audioContext.resume();
   if (audioContext.state === "running") return audioContext.suspend();
 };
@@ -31,6 +27,7 @@ const interactive = () => {
   document.body.addEventListener("click", startStop);
 };
 
+let str;
 navigator.mediaDevices
   .getUserMedia({ audio: true, video: false })
   .then((stream) => audioContext.createMediaStreamSource(stream))
