9351748c85faec3f1c2f3640f88e1cc6d40525bb
Chris Smith <chrisronsmith@gmail.com>
2021-11-11 16:36:22 -0500

    Use JSDoc/TypeScript type annotations

--- path.js
+++ path.js
@@ -5,6 +5,12 @@ const lineTo = (path, { time, amplitude }) => path + `L ${time}, ${amplitude} `;
 
 const normalize = (float) => (float + 0.5) * 128;
 
+/**
+ * Given an array of 32 bit floating point numbers, produces a string path definition for passing to the 'd' attribute of svg path elements.
+ *
+ * @param {Float32Array} floats
+ * @returns {string}
+ */
 export const toPath = (floats) => {
   const [first, ...rest] = floats.map(normalize);
   const startingPoint = move("", { time: 0, amplitude: first });
