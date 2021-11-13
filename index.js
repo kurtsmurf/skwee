d6c24a83b922ef24b1cbc8b6c96513ccc207598c
Chris Smith <chrisronsmith@gmail.com>
2021-11-13 15:49:17 -0500

    Initialise AudioContext after user interaction

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-// @ts-check
+//@ts-check
 import { toPath } from "./path.js";
 
-const audioContext = new AudioContext();
-const analyser = audioContext.createAnalyser();
-analyser.fftSize = 128;
+const start = () => {
+  const audioContext = new AudioContext();
+  const analyser = audioContext.createAnalyser();
+  const scopePath = document.querySelector("[scope-path]");
 
-const samplesHolder = new Float32Array(analyser.fftSize);
-const scopePath = document.querySelector("[scope-path]");
+  const fftSize = 128;
+  const samplesHolder = new Float32Array(fftSize);
+  analyser.fftSize = fftSize;
 
-const draw = () => {
-  analyser.getFloatTimeDomainData(samplesHolder);
-  scopePath.setAttribute("d", toPath(samplesHolder));
+  const draw = () => {
+    analyser.getFloatTimeDomainData(samplesHolder);
+    scopePath.setAttribute("d", toPath(samplesHolder));
+  };
+
+  const loop = () => {
+    draw();
+    requestAnimationFrame(loop);
+  };
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then((stream) => audioContext.createMediaStreamSource(stream))
+    .then((streamSource) => streamSource.connect(analyser))
+    .then(loop);
 };
 
-const loop = () => {
-  draw();
-  requestAnimationFrame(loop);
-};
-
-const startStop = () => {
-  if (audioContext.state === "suspended") return audioContext.resume();
-  if (audioContext.state === "running") return audioContext.suspend();
-};
-
-const interactive = () => {
-  document.body.addEventListener("click", startStop);
-};
-
-let str; // Hold a reference to stream source - this seems to keep it active
-navigator.mediaDevices
-  .getUserMedia({ audio: true, video: false })
-  .then((stream) => audioContext.createMediaStreamSource(stream))
-  .then((streamSource) => {
-    str = streamSource;
-    streamSource.connect(analyser)
-  })
-  .then(loop)
-  .then(interactive);
-
-let slider = document.createElement('input')
-slider.type = 'range'
-slider.addEventListener('input', e => {
-    document.body.style.setProperty("--base-hue", e.target.value * 3.6);
-})
-document.body.appendChild(slider)
+document.body.addEventListener("click", () => {
+  start();
+  document.body.removeEventListener("click", start);
+});
