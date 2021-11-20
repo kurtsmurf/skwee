9ae8efa3343e9abb57b2832a02d80188b5eb2444
Kurt <chrisronsmith@gmail.com>
2021-11-20 16:45:05 -0500

    add spread control on y axis

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 import { start } from "./start.js";
 
 document.body.style.setProperty("--base-hue", `${Math.random() * 360}`);
-
-// --
+document.body.style.setProperty("--spread", `${Math.random() * 720}`);
 
 const prompt = document.querySelector("figure");
 prompt.addEventListener("pointerdown", () => {
@@ -11,23 +10,41 @@ prompt.addEventListener("pointerdown", () => {
   prompt.style.setProperty("display", "none");
 });
 
-// --
-
-let dragStart;
+/** @type {number} */
 let baseHue;
-
-const drag = (e) => {
-  const newValue = (baseHue + (e.x - dragStart) / 5) % 360;
+const updateBaseHue = (delta) => {
+  const newValue = (baseHue + delta / 5) % 360;
   document.body.style.setProperty("--base-hue", newValue.toString());
 };
-const startDrag = (e) => {
-  dragStart = e.x;
+const startBaseHueUpdate = () => {
   baseHue = parseFloat(document.body.style.getPropertyValue("--base-hue"));
+};
+
+/** @type {number} */
+let spread;
+const updateSpread = (delta) => {
+  let newValue = (spread + delta / 5) % 720;
+  document.body.style.setProperty("--spread", newValue.toString());
+};
+const startSpreadUpdate = () => {
+  spread = parseFloat(document.body.style.getPropertyValue("--spread"));
+};
+
+/** @type {{x: number, y:number}} */
+let dragStart;
+const drag = (e) => {
+  updateBaseHue(e.x - dragStart.x);
+  updateSpread(e.y - dragStart.y);
+};
+const startDrag = (e) => {
   document.body.addEventListener("pointermove", drag);
+  dragStart = { x: e.x, y: e.y };
+  startBaseHueUpdate();
+  startSpreadUpdate();
 };
 const stopDrag = () => {
-  dragStart = undefined;
   document.body.removeEventListener("pointermove", drag);
+  dragStart = undefined;
 };
 
 document.body.addEventListener("pointerdown", startDrag);
